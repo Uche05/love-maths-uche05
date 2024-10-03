c100f538c3d85dae00faf66cbbca47175ccc0159
Uche05 <uchekpadeuwa@gmail.com>
2024-10-03 13:18:56 +0100

    add script for functionality

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function runGame(){
+
+}
+
+function checkAnswer(){
+
+}
+
+function calcCorrectAns(){
+
+}
+
+function incrementScore(){
+
+}
+
+function incrementWrongAnswer(){
+
+}
+
+function displayAddQuestion(){
+
+}
+
+function displaySubtractQuestion(){
+
+}
+
+function displayMultipleQuestion(){
+    
+}
