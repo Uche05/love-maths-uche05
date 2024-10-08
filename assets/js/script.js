ddda4a7d10be0d80c4171ef0d030e6b5112cea16
Uche05 <uchekpadeuwa@gmail.com>
2024-10-08 19:15:05 +0100

    add script to display add questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,24 +7,56 @@ document.addEventListener("DOMContentLoaded", function(){
     for(let button of buttons){
         button.addEventListener('click', function(){
             if(this.getAttribute('data-type') === 'submit'){
-                alert('You clciked submit!');
+                checkAnswer();
             } else{
                 let gameType = this.getAttribute('data-type');
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
 
         })
     }
+
+    runGame("addition");
 })
-function runGame(){
+/**
+ *The main game "loop", called when the script is first loaded
+ *and  after the user's answer has been processed
+ */
+function runGame(gameType) {
+
+    //creates two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random()*25) +1;
+    let num2 = Math.floor(Math.random()*25)+1;
+
+    if (gameType === "addition"){
+        displayAddQuestion(num1,num2);
+    } else{
+            alert(`Unknown game type: ${gametype}`);
+            throw `Unknown game type: ${gameType}.Aborting!`;
+        }
+    
 
 }
 
 function checkAnswer(){
 
 }
-
+/**
+ * Gets the operands (the numbers) and the operator(plus, .minus etc)
+ * directly from the dom, and returns the correct answer.
+ */
 function calcCorrectAns(){
+
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById('operator').innerText;
+
+    if(operator === '+'){
+        return [operand1 + operand2, "addition"];
+    } else{
+        alert(`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}, Aborting!`;
+    }
 
 }
 
@@ -36,8 +68,10 @@ function incrementWrongAnswer(){
 
 }
 
-function displayAddQuestion(){
-
+function displayAddQuestion(operand1, operand2){
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = '+';
 }
 
 function displaySubtractQuestion(){
@@ -45,5 +79,9 @@ function displaySubtractQuestion(){
 }
 
 function displayMultipleQuestion(){
+
+}
+
+function displayDivideQuestion(){
 
 }
