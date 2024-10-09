695e3218a947ae9192b1b511a4b48255500c21ed
Uche05 <uchekpadeuwa@gmail.com>
2024-10-09 17:49:57 +0100

    correctly make multiply game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -73,7 +73,7 @@ function calcCorrectAns(){
     if(operator === '+'){
         return [operand1 + operand2, "addition"];
     } else if(operator === 'x'){
-        return [operand1 + operand2, "multiply"];
+        return [operand1 * operand2, "multiply"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}, Aborting!`;
